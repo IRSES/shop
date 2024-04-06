7ae37581962b838e390c50f3221dc858fceb9188
IRSES <barmen3456besdna@gmail.com>
2024-04-06 20:58:35 +0300

    [AS-2] - Created pages

--- main/models.py
+++ main/models.py
@@ -1,3 +1,10 @@
+# models.py
+
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='static/img/')  # Путь для сохранения изображений товаров
--- main/views.py
+++ main/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import *
 
 
-def index(request):
-    return HttpResponse("<h1>Hello World</h1>")
+def home(request):
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, 'main/home.html', context)
 
 
 def about(request):
-    return HttpResponse("<h1>About us</h1>")
+    return render(request, 'main/about.html')
